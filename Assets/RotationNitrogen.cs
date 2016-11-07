c5ee339b87eb6c906ce325cc9fd4f448b0f29a2d
Margrethe Moxnes <margrethe@moxnesdesign.no>
2016-11-07 13:04:38 +0100

    Fixed materials

--- Assets/RotationNitrogen.cs
+++ Assets/RotationNitrogen.cs
@@ -2,17 +2,9 @@
 using System.Collections;
 
 public class RotationNitrogen : MonoBehaviour {
-
-    int y;
-
-	// Use this for initialization
-	void Awake () {
-	
-	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        y++;
         gameObject.transform.Rotate(Vector3.right, 1f);
     }
 }
