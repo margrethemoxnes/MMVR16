7fd8d6dd248918049c01dbb5d3790680af855880
Margrethe Synnøve Moxnes <msmoxn03@student.uia.no>
2016-11-26 10:42:03 +0100

    When Nitrogen Lid is removed, no sound will play

--- Assets/Scripts/NitrogenScript.cs
+++ Assets/Scripts/NitrogenScript.cs
@@ -10,22 +10,17 @@ public class NitrogenScript : MonoBehaviour {
     float nitrogenX;
     float nitrogenZ;
 
-    private AudioSource source;
-    public AudioClip nitrogenIsOpen;
-
     void Awake() {
         nitrogenX = gameObject.transform.position.x;
         nitrogenZ = gameObject.transform.position.z;
-        source = GetComponent<AudioSource>();
     }
 
     void OnCollisionExit(Collision col)
     {
         if (col.gameObject == lid)
         {
-            //Spill av lyd om nitrogen åpen.
+            //Start liten røyk animasjon når lokket er tatt av.
             if (nitrogenOpen == false) {
-                source.PlayOneShot(nitrogenIsOpen, 1F);
                 nitrogenOpen = true;
                 Instantiate(nitrogenGasContainer, new Vector3(nitrogenX, 10.624f, nitrogenZ), Quaternion.identity);
             }
